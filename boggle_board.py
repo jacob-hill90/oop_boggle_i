1998d4e200a31208ad417bf27a911d552ff616a7
Jacob Hill <hilljake93@gmail.com>
2022-06-02 17:06:47 -0500

    needs work

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,24 @@
+import random
+import string
+
 class BoggleBoard:
   
-  def __init__(self):
+  def __init__(self, length, width,):
+    self.length = length
+    self.width = width
   
+  def new(self):
+    print("_" * self.length)
+    print("_" * self.length)
+    print("_" * self.length)
+    print("_" * self.length)
 
   def shake(self):
+    print(f"{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}")
+    print(f"{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}")
+    print(f"{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}")
+    print(f"{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}")
+
+board = BoggleBoard(4,4)
+board.new()
+board.shake()
